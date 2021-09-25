d2307702c91eb22c0d315b69e1b026e10e229c6d
Максим <PotapovWebDev@gmail.com>
2021-09-25 22:45:52 +0300

    practic n2

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const number0fFilms = +prompt("Сколько фильмов вы уже посмотрели", "Напишите число");
+let number0fFilms = +prompt("Сколько фильмов вы уже посмотрели", "2");
+
+if (number0fFilms < 10) {
+    alert('посмотрел слишком мало фильмов');
+} else if (number0fFilms > 10 || number0fFilms < 30) {
+    alert('классический зритель');
+} else if (number0fFilms < 30) {
+    alert(' вы киноман');
+} else {
+    alert('произошла ошибка');
+    number0fFilms = +prompt("Сколько фильмов вы уже посмотрели", "Напишите число");
+}
 
 const personalMovieDB = {
     count: [number0fFilms],
@@ -8,11 +19,41 @@ const personalMovieDB = {
     privat: false  
 };
 
-const a = prompt("один из последних просмотреных фильмов", "название фильма");
-const b = prompt("на сколько оцениет его?", "от 1 до 10");
-const c = prompt("один из последних просмотреных фильмов", "название фильма");
-const d = prompt("на сколько оцениет его?", "от 1 до 10");
+for (let i = 0; i < 2; i++) {
+    let a = prompt("один из последних просмотреных фильмов", ""),
+        b = prompt("на сколько оцениет его?", "");
 
-personalMovieDB.movies[a] = b;
+    if( a != null && b != null && a != '' && b != '' && a.length < 5) {
+        personalMovieDB.movies[a] = b;
+    }   else {
+        i--;
+    }
+}
+
+
+/*  `````````````````````````````` \/ проверка на других циклах \/  ````````````````````````` */
+
+/* let i = 0;
+
+    while ( i < 2) {
+        let a = prompt("один из последних просмотреных фильмов", "");
+        let b = prompt("на сколько оцениет его?", "");
+        i++;
+        personalMovieDB.movies[a] = b;
+        
+    } */
+
+
+/* let i = 0;
+    do {
+        let a = prompt("один из последних просмотреных фильмов", "");
+        let b = prompt("на сколько оцениет его?", "");
+        i++;
+        personalMovieDB.movies[a] = b;
+    }
+    while (i < 2); */
+
+/*  `````````````````````````` /\ ````````````````````````````` */
+
 
 console.log(personalMovieDB);
